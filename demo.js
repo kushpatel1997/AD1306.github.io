ee7f6916e42bc6f28eda58437505419add2442a1
Aditya Dave <adityadave.dave89@gmail.com>
2016-10-05 00:34:06 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -5,8 +5,8 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    dotColor: '#ffffff',
+    lineColor: '#fffffc'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
